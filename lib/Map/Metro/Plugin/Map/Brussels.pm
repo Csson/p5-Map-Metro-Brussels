5483d5856fdcd55edc7b554669ef746bcf2238a0
Erik Carlsson <info@code301.com>
2015-01-06 21:12:14 +0100

    Documentation

--- lib/Map/Metro/Plugin/Map/Brussels.pm
+++ lib/Map/Metro/Plugin/Map/Brussels.pm
@@ -36,6 +36,13 @@ See L<Map::Metro> for usage information.
 
 =head1 Status
 
-...
+This map includes:
+=for :list
+* The four metro lines I<L1>, I<L2>, I<L5> and I<L6> [L<wikipedia|https://en.wikipedia.org/wiki/Brussels_Metro>]
+* The I<T3> tram line, but only between I<Gare du Nord/Noordstation> and I<Albert> [L<wikipedia|https://en.wikipedia.org/wiki/Brussels_tram_route_3>]
+
+See L<Map::Metro::Plugin::Map::Brussels::Lines>
+
+=for HTML <p><a href="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Brussels/master/static/images/brussels.png"><img src="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Brussels/master/static/images/brussels.png" style="max-width: 600px" /></a></p>
 
 =cut
