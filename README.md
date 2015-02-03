57e6c35a1e02f1e78a17b5b0dc9bb19736ddb66d
Erik Carlsson <info@code301.com>
2015-02-03 22:21:25 +0100

    Improved synopsis. Minor cleanups.

--- README.md
+++ README.md
@@ -4,12 +4,16 @@ Map::Metro::Plugin::Map::Brussels - Map::Metro map for Brussels
 
 # VERSION
 
-Version 0.0001, released 2015-01-07.
+Version 0.1001, released 2015-02-03.
 
 # SYNOPSIS
 
     use Map::Metro;
     my $graph = Map::Metro->new('Brussels')->parse;
+
+Or:
+
+    map-metro.pl route Brussels "Gare de l'Ouest" 'Centraal Station'
 
 # DESCRIPTION
 
@@ -17,12 +21,10 @@ See [Map::Metro](https://metacpan.org/pod/Map::Metro) for usage information.
 
 # Status
 
-This map includes:
+This map [contains](https://metacpan.org/pod/Map::Metro::Plugin::Map::Brussels::Lines):
 
 - The four metro lines _L1_, _L2_, _L5_ and _L6_ \[[wikipedia](https://en.wikipedia.org/wiki/Brussels_Metro)\]
 - The _T3_ tram line, but only between _Gare du Nord/Noordstation_ and _Albert_ \[[wikipedia](https://en.wikipedia.org/wiki/Brussels_tram_route_3)\]
-
-See [Map::Metro::Plugin::Map::Brussels::Lines](https://metacpan.org/pod/Map::Metro::Plugin::Map::Brussels::Lines)
 
 # SOURCE
 
--- lib/Map/Metro/Plugin/Map/Brussels.pm
+++ lib/Map/Metro/Plugin/Map/Brussels.pm
@@ -5,6 +5,7 @@ use warnings;
 package Map::Metro::Plugin::Map::Brussels;
 
 # VERSION
+# ABSTRACT: Map::Metro map for Brussels
 
 use Moose;
 with 'Map::Metro::Plugin::Map';
@@ -21,8 +22,6 @@ sub map_package {
 
 1;
 
-# ABSTRACT: Map::Metro map for Brussels
-
 __END__
 
 =pod
@@ -32,19 +31,21 @@ __END__
     use Map::Metro;
     my $graph = Map::Metro->new('Brussels')->parse;
 
+Or:
+
+    map-metro.pl route Brussels "Gare de l'Ouest" 'Centraal Station'
+
 =head1 DESCRIPTION
 
 See L<Map::Metro> for usage information.
 
 =head1 Status
 
-This map includes:
+This map L<contains|Map::Metro::Plugin::Map::Brussels::Lines>:
 
 =for :list
 * The four metro lines I<L1>, I<L2>, I<L5> and I<L6> [L<wikipedia|https://en.wikipedia.org/wiki/Brussels_Metro>]
 * The I<T3> tram line, but only between I<Gare du Nord/Noordstation> and I<Albert> [L<wikipedia|https://en.wikipedia.org/wiki/Brussels_tram_route_3>]
-
-See L<Map::Metro::Plugin::Map::Brussels::Lines>
 
 =for HTML <p><a href="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Brussels/master/static/images/brussels.png"><img src="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Brussels/master/static/images/brussels.png" style="max-width: 600px" /></a></p>
 
